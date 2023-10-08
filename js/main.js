8f4ab18a6edb84f33729e81bfa1568cabd894a07
CARINHA0800 <phoscoli1@gmail.com>
2023-10-08 16:04:58 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import '../css/style.css';
+import '../css/componentes/header.css';
+import '../css/modern-normalise.css';
+import '../css/componentes/perfil.css';
+import '../css/componentes/sobre.css';
+import '../css/utilidades.css';
