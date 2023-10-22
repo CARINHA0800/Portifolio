f7aa43c532e498bc523bfad9e7febc3200841c66
CARINHA0800 <phoscoli1@gmail.com>
2023-10-22 20:31:56 -0300

    second

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ import '../css/modern-normalise.css';
 import '../css/componentes/perfil.css';
 import '../css/componentes/sobre.css';
 import '../css/utilidades.css';
+import '../css/componentes/projetos.css'
+import '../css/componentes/footer.css'
+import '../css/componentes/mobile-nav.css'
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+mobileNav();
+darkMode();
